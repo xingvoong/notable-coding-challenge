b4bbd13301b5634bc5bc96ed43e8a71e69a86de2
Xing Voong <xingvoong@gmail.com>
2021-11-15 17:18:58 -0800

    add routes

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const controller = require('./controller');
 
 router.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+router.get('/users', controller.users.get);
 
 module.exports = router;

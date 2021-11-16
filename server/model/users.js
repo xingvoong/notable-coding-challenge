0fc0d7e18860212da629a455fad199e1c20bb5b3
Xing Voong <xingvoong@gmail.com>
2021-11-16 12:30:22 -0800

    write getUserByID model

--- server/model/users.js
+++ server/model/users.js
@@ -11,8 +11,44 @@ const getAllUsers = async () => {
   } finally {
     client.release();
   }
-}
+};
+
+// const getOneUser = async (username) => {
+//   // const value = username
+//   // const query = `select * from users where username = ${1}`
+//   // console.log("query", query)
+//   const value = userID
+//   console.log("value", value)
+//   const query = 'select * from users where id = $1';
+
+//   try {
+//     return await client.query(query, value);
+//   } catch (err) {
+//     return err
+//   } finally {
+//     client.release();
+//   }
+// };
+
+const getUserByID = async (userID) => {
+  // const value = username
+  // const query = `select * from users where username = ${1}`
+  // console.log("query", query)
+  const value = [userID]
+  console.log("value", value)
+  const client = await pool.connect();
+  const query = 'select * from users where id = $1';
+  console.log("query", query)
+
+  try {
+    return await client.query(query, value);
+  } catch (err) {
+    return err
+  } finally {
+    client.release();
+  }
+};
 
 module.exports = {
-  getAllUsers
+  getAllUsers, getUserByID
 };

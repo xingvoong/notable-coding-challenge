7add2285404cab7fd91de606041dcbc3abedc4e6
Xing Voong <xingvoong@gmail.com>
2021-11-16 13:59:38 -0800

    implement getUserByName controller

--- server/controller/users.js
+++ server/controller/users.js
@@ -6,16 +6,16 @@ module.exports = {
     .then((result) => res.status(200).send(result.rows))
     .catch((err) => { res.status(404).send(err); });
   },
-  // getOneUser: (req, res) => {
-  //   const {username} = req.params;
-  //   model.users.getOneUser(username)
-  //   .then((result) => res.status(200).send(result.rows))
-  //   .catch((err) => { res.status(404).send(err); });
-  // },
+
+  getUserByName: (req, res) => {
+    const {username} = req.params;
+    model.users.getUserByName(username)
+    .then((result) => res.status(200).send(result.rows))
+    .catch((err) => { res.status(404).send(err); });
+  },
 
   getUserById: (req, res) => {
     const {userID} = req.params;
-    console.log("hello?")
     model.users.getUserByID(userID)
     .then((result) => res.status(200).send(result.rows))
     .catch((err) => { res.status(404).send(err); });

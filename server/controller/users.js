465da513806dff60b40c87b704618e960d1e635a
Xing Voong <xingvoong@gmail.com>
2021-11-16 12:29:50 -0800

    write getUserByID controller

--- server/controller/users.js
+++ server/controller/users.js
@@ -1,8 +1,22 @@
 const model = require('../model');
 
 module.exports = {
-  get: (req, res) => {
+  getAllUsers: (req, res) => {
     model.users.getAllUsers()
+    .then((result) => res.status(200).send(result.rows))
+    .catch((err) => { res.status(404).send(err); });
+  },
+  // getOneUser: (req, res) => {
+  //   const {username} = req.params;
+  //   model.users.getOneUser(username)
+  //   .then((result) => res.status(200).send(result.rows))
+  //   .catch((err) => { res.status(404).send(err); });
+  // },
+
+  getUserById: (req, res) => {
+    const {userID} = req.params;
+    console.log("hello?")
+    model.users.getUserByID(userID)
     .then((result) => res.status(200).send(result.rows))
     .catch((err) => { res.status(404).send(err); });
   }

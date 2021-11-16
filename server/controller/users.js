8ea22f5c3a5a98d1170c787610ada52880a161c4
Xing Voong <xingvoong@gmail.com>
2021-11-16 14:42:26 -0800

    write controller for createUser

--- server/controller/users.js
+++ server/controller/users.js
@@ -19,5 +19,17 @@ module.exports = {
     model.users.getUserByID(userID)
     .then((result) => res.status(200).send(result.rows))
     .catch((err) => { res.status(404).send(err); });
+  },
+
+  createUser: (req, res) => {
+    const {id, username, user_email} = req.body;
+    console.log("id", id)
+    console.log("username", username)
+    console.log("user_email", user_email)
+    model.users.createUser([id, username, user_email])
+    .then(res.status(200).send(`create a new user with id: ${id}, username: ${username}, user_email: ${user_email}`))
+    .catch((err) => { res.status(404).send(err); });
   }
 };
+
+// insert into "users" (id, username, user_email) values ($1, $2, $3)

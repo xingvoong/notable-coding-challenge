96951b2e6a876569978a7ff7d62f71cd0a10d016
Xing Voong <xingvoong@gmail.com>
2021-11-16 15:25:45 -0800

    implement controller for delete user

--- server/controller/users.js
+++ server/controller/users.js
@@ -23,13 +23,15 @@ module.exports = {
 
   createUser: (req, res) => {
     const {id, username, user_email} = req.body;
-    console.log("id", id)
-    console.log("username", username)
-    console.log("user_email", user_email)
     model.users.createUser([id, username, user_email])
     .then(res.status(200).send(`create a new user with id: ${id}, username: ${username}, user_email: ${user_email}`))
     .catch((err) => { res.status(404).send(err); });
+  },
+
+  deleteUser: (req, res) => {
+    const {id} = req.params;
+    model.users.deleteUser(id)
+    .then(res.status(200).send(`delete a user with id: ${id}`))
+    .catch((err) => { res.status(404).send(err); });
   }
 };
-
-// insert into "users" (id, username, user_email) values ($1, $2, $3)

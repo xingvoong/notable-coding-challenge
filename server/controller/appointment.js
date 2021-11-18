aef24c837a2352ba688a7400c4edd46e16c8f995
Xing Voong <xingvoong@gmail.com>
2021-11-17 17:47:18 -0800

    write controller for delete request

--- server/controller/appointment.js
+++ server/controller/appointment.js
@@ -6,5 +6,11 @@ module.exports = {
     model.appointment.getAppointment(doctor_id, date_time)
     .then((result) => res.status(200).send(result.rows))
     .catch((err) => { res.status(404).send(err); });
+  },
+  deleteAppointment: (req, res) => {
+    const {id} = req.params;
+    model.appointment.deleteAppointment(id)
+    .then(res.status(200).send(`delete appointment with id: ${id}`))
+    .catch((err) => { res.status(404).send(err); });
   }
 };
